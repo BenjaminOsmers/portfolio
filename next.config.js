8868e5c14e5dab11c2f81367339461462438189d
Benjamin Osmers <73130428+BenjaminOsmers@users.noreply.github.com>
2022-04-28 09:25:33 +0200

    ⚡️ Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	rewrites: async () => [
+		{
+			source: '/public/project_structure.html',
+			destination: '/pages/api/myMindmap.js',
+		},
+	],
 }
 
 module.exports = nextConfig
